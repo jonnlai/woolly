2f3914afef8914fdb075115304ade933492bc7b4
Jonna Laine <jonna.jp.laine@gmail.com>
2024-11-18 17:14:52 +0000

    feat: add toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
 from products.models import Product
 
 
@@ -22,6 +23,7 @@ def add_to_bag(request, item_id):
     """
      Adds a quantity of the specified product to the shopping bag 
      """
+    product = get_object_or_404(Product, pk=item_id)
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -29,8 +31,10 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
 
