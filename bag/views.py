dc8e6775121d90cca9c77480cffe23c14c00c40c
Jonna Laine <jonna.jp.laine@gmail.com>
2024-11-18 17:21:54 +0000

    feat: add messages when bag is updated

--- bag/views.py
+++ bag/views.py
@@ -51,8 +51,10 @@ def adjust_bag(request, item_id):
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['bag'] = bag
 
@@ -64,9 +66,12 @@ def remove_from_bag(request, item_id):
     Remove the item from the shopping bag
     """
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
