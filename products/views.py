30ad689bf6ed98460cfba1ba90cf58e83f80b4d8
Jonna Laine <jonna.jp.laine@gmail.com>
2024-11-27 22:13:54 +0000

    feat: add functionality for admin to delete producst

--- products/views.py
+++ products/views.py
@@ -112,9 +112,9 @@ def add_product(request):
     if request.method == 'POST':
         product_form = ProductForm(request.POST, request.FILES)
         if product_form.is_valid():
-            product_form.save()
+            product = product_form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -162,3 +162,19 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    Delete a product
+
+    **Context**
+
+    ``product``
+        An instance of :model:`products.Product`
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    
+    return redirect(reverse('products'))
