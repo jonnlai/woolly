93b4e0bc818c4f72589c1164f6d3b7b226d9df75
Jonna Laine <jonna.jp.laine@gmail.com>
2024-11-27 17:43:41 +0000

    feat: add order history and update checkout views

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     """
@@ -27,13 +29,28 @@ def profile(request):
             messages.success(request, 'Profile updated successfully')
     
     form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
+    orders = profile.orders.all().order_by('-date')
 
     template = 'profiles/profile.html'
-    context = {
-        'profile': profile,
-        'form': form,
-        'orders': orders,
-        'on_profile_page': True}
+    context = {'profile': profile,
+               'form': form,
+               'orders': orders,
+               'on_profile_page': True}
 
     return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+    
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    return render(
+        request,
+        'checkout/checkout_success.html',
+        {'order': order,
+        'from_profile': True,}
+    )
