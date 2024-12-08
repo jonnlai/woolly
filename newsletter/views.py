5e217aefdfbc1c7d776cf9a15cf73d61ab87ea7b
Jonna Laine <jonna.jp.laine@gmail.com>
2024-12-08 20:19:34 +0000

    feat: add welcome email to newsletter subscribers

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+from django.conf import settings
+from django.template.loader import render_to_string
 
 from .models import Subscriber
 from .forms import SubscribeForm
@@ -15,7 +18,9 @@ def subscribe(request):
     ``subscribe_form``
         An instance of of :form:`newsletter.SubscriberForm`
     """
-
+    # Inspiration taken from:
+    # Codemy - https://www.youtube.com/watch?v=xNqnHmXIuzU and 
+    # Joshyvibes - https://www.youtube.com/watch?v=LL6qXu8FmVo
     subscribe_form = SubscribeForm(data=request.POST or None)
 
     if request.method == 'POST':
@@ -26,12 +31,24 @@ def subscribe(request):
                 f"{subscriber.email} has been added! \
                     Check your emails for some woolly good news!"
             )
+            # send a welcome email to the user
+            email_subject = 'Welcome to Woolly family!'
+            recipient = subscriber.email
+            content = render_to_string(
+            'newsletter/confirmation_emails/welcome_email.txt')
+
+            send_mail(
+                email_subject,
+                content,
+                settings.DEFAULT_FROM_EMAIL,
+                ['recipient'],
+            )
         else:
             messages.error(
                 request,
-                f"Error adding {subscriber.email}. Please check that you \
-                    spelt the address correctly \
-                    and that you have subscribed already!"
+                "An error has occured! Please check that you \
+                    spelt your email correctly \
+                    and that you have not subscribed already!"
             )
 
         # Get the url of the previous page the user was on
