e7948e4595532c2ae1c499a77e3d2128b425eb02
Jonna Laine <jonna.jp.laine@gmail.com>
2024-12-10 22:09:22 +0000

    feat: style product detail pg and disable submit btn if product not in stock

--- reviews/forms.py
+++ reviews/forms.py
@@ -7,3 +7,12 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['review_title', 'review_content', 'product_rating']
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add text-muted class
+        """
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'text-muted my-2'
